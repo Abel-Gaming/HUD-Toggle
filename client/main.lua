8be136b6cc5bf330cdfdc6cc9628635c7bdb213e
Abel Gaming <lcpdpolice4939@gmail.com>
2021-06-05 17:17:09 -0400

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,4 +1,5 @@
 ESX              = nil
+local hudHidden = false
 
 Citizen.CreateThread(function()
 	while ESX == nil do
@@ -8,11 +9,22 @@ Citizen.CreateThread(function()
 end)
 
 RegisterCommand("hidehud", function(source, args, rawCommand)
-	ESX.UI.HUD.SetDisplay(0)
-	ESX.ShowNotification("~r~HUD Hidden", false, true, 130)
+	hudHidden = true
+	TriggerEvent("swt_notifications:Icon","HUD Hidden","top-right",2500,"red-10","white",true,"mdi-earth-off")
 end, false)
 
 RegisterCommand("showhud", function(source, args, rawCommand)
-	ESX.UI.HUD.SetDisplay(100)
-	ESX.ShowNotification("~g~HUD Shown", false, true, 210)
+	hudHidden = false
+	TriggerEvent("swt_notifications:Icon","HUD Shown","top-right",2500,"green-10","white",true,"mdi-earth")
 end, false)
+
+Citizen.CreateThread(function()
+	while true do
+		Citizen.Wait(0)
+		if hudHidden then
+			ESX.UI.HUD.SetDisplay(0.0)
+		else
+			ESX.UI.HUD.SetDisplay(1.0)
+		end
+	end
+end)
